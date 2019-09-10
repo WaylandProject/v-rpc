ac69d13e9f2f4b1d48add6fbe65414348cfd431e
TheMysteriousVincent <agpheaddikilla@yahoo.de>
2019-09-11 01:46:21 +0200

    update id generation, add browser registry docs

--- src/js/lib/browserRegistry.ts
+++ src/js/lib/browserRegistry.ts
@@ -1,11 +1,24 @@
 
+/**
+ * This registry is used to maintain the various browser data objects from different GTAV multiplayer providers.
+ *
+ * @remarks
+ * Currently, only RageMP is supported
+ */
 class BrowserRegistry<T> {
   private browserDict: Map<number, T> = new Map<number, T>();
+  private idCounter = 0;
 
   private generateUid(): number {
-    return Math.round(2147483648 - (Math.random() * 4294967296));
+    return ++this.idCounter;
   }
 
+  /**
+   * Registers a browser with an unique id
+   * @param browser The browser to register
+   *
+   * @returns The unique id of the browser
+   */
   public register(browser: T): number {
     for (const n of Array.from(this.browserDict.entries())) {
       if (n[1] === browser) {
@@ -18,6 +31,11 @@ class BrowserRegistry<T> {
     return uid;
   }
 
+  /**
+   * Unregisters a browser
+   *
+   * @param browser The browser to unregister
+   */
   public unregister(browser: T): void {
     Array.from(this.browserDict.entries()).forEach(n => {
       if (n[1] === browser) {
@@ -26,6 +44,11 @@ class BrowserRegistry<T> {
     });
   }
 
+  /**
+   * Returns the unique identifier of a browser object
+   *
+   * @param browser The browser object to get the uid from
+   */
   public getId(browser: T): number | undefined {
     for (const n of Array.from(this.browserDict.entries())) {
       if (n[1] === browser) {
@@ -36,6 +59,11 @@ class BrowserRegistry<T> {
     return undefined;
   }
 
+  /**
+   * Returns the browser attached to the unique id.
+   *
+   * @param id The unique identifier of a browser.
+   */
   public getBrowser(id: number): T | undefined {
     return this.browserDict.get(id);
   }
