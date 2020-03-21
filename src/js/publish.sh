3ca2f873e63f11d2c6112eb29b2dd064853109f4
TheMysteriousVincent <agpheaddikilla@yahoo.de>
2020-03-21 16:31:24 +0100

    split publish script into seperate files

--- src/js/publish.sh
+++ src/js/publish.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 
-rm -Rf dist/
-./node_modules/.bin/webpack --mode production
-
-cp -rf package.json ../../README.md ../../LICENSE dist/
-echo "{\"main\": \"index.js\"}" >> dist/ragemp/cef/package.json
-echo "{\"main\": \"index.js\"}" >> dist/ragemp/client/package.json
-echo "{\"main\": \"index.js\"}" >> dist/ragemp/server/package.json
-
-mkdir -p dist/lib
-
-mv -f lib/*.d.ts dist/lib/
-mv -f ragemp/cef/index.d.ts dist/ragemp/cef/
-mv -f ragemp/client/index.d.ts dist/ragemp/client/
-mv -f ragemp/server/index.d.ts dist/ragemp/server/
+./build.sh
 
 cd dist/
 
